d8c07a2043cc09b2987f5ea42d6f9d99f149df47
shullmb <shullmb@gmail.com>
2018-05-29 16:56:30 -0700

    ToDo List working - requirements met

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
+//function to generate styled list items
+var htmlToDoGenerator = function(listItem,dueDate){
+    return "<div class='item'> <h4>" + listItem+ "<br> due by: " + dueDate+ "</h4><button type='button' class='done'>Done</button><div>";
+}
+
+// function to append list item to body of page
+var addToDo = function(e) {
+    e.preventDefault();
+    // get data in useable format
+    var toDoData = $('form').serializeArray();
+
+    // set up vars for easier handling
+    var listItem = toDoData[0].value;
+    var formDate = toDoData[1].value.split('-');
+    var dueDate = formDate[1] + "/" + formDate[2]+"/"+formDate[0];
+    
+    var divToAppend = htmlToDoGenerator(listItem, dueDate)
+    // set
+    $('.list').append( divToAppend);
+
+    // clear out input fields
+    for (let i = 0; i < $('input').length; i++ ){
+        $('input')[i].value = "";
+    }
+
+    // wire up event handler to delete item
+    $('div.item').on("click", "button.done", markDone);
+
+    //focus back on 'Thing to do'
+    $('input')[0].focus();
+}
+
+var markDone = function() {
+    $(this).parent().remove();
+}
+
 $(document).ready(function () {
-    console.log('jQuery working')
-    $('
+    $('input')[0].focus();
+    $('form').submit(addToDo);
 })
